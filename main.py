6e54dc0e9f709a342641bc0029f323ea558c7c64
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-10-10 18:20:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -196,7 +196,7 @@ class GUI:
 
                 # render random view
                 ver = np.random.randint(min_ver, max_ver)
-                hor = 180 - 360 / self.train_steps * iter_num
+                hor = 180 - 360 / iters * iter_num
                 print(hor)
                 radius = 0
 
