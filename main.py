84ebbc233165aa9e2af1bba7837209f94d9e4a87
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-10-11 10:11:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -197,6 +197,7 @@ class GUI:
                 # render random view
                 ver = np.random.randint(min_ver, max_ver)
                 hor = 360 / iters * (iter_num // 4)+ (iter_num % 4) * 90
+                print(f"hor: {hor}")
                 radius = 0
 
                 vers.append(ver)
