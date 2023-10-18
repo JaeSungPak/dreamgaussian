6ae7e154897a11c5487499d16aaeac1d49d6390d
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-10-18 17:30:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ class GUI:
                 
                 # self.guidance_zero123.get_img_embeds(self.input_img_torch)
                 
-                loss_image_back = 3000 * step_ratio * F.mse_loss(image_back.to(torch.float32), self.input_img_back.to(torch.float32))
+                loss_image_back = 3000 * step_ratio * F.mse_loss(image_back.float(), self.input_img_back.float())
                 
                 loss = loss + loss_image + loss_image_back
                 
