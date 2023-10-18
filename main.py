36042df837f0f12d62a6b9ab7e6b06bc55b898fd
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-10-18 16:20:52 +0900

    back image

--- main.py
+++ main.py
@@ -158,8 +158,7 @@ class GUI:
         if self.input_img is not None:
             self.input_img_torch = torch.from_numpy(self.input_img).permute(2, 0, 1).unsqueeze(0).to(self.device)
             self.input_img_torch = F.interpolate(self.input_img_torch, (self.opt.ref_size, self.opt.ref_size), mode="bilinear", align_corners=False)
-            self.input_img_back = self.guidance_zero123.pipe._encode_image(self.input_img_torch, 180, 0, 0, self.device, 1, False)
-            #self.input_img_back = self.guidance_zero123.pipe.decode_latents(self.input_img_back)
+            self.input_img_back = self.guidance_zero123.back_image(self.input_img_torch, 180, 0, 0)
             
             print(f"zero123: {self.input_img_back.shape}")
 
