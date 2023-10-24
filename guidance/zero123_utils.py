aae3047c9fdab795ed7dc3c4749831621602ada6
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-10-24 11:45:03 +0900

    Update zero123_utils.py

--- guidance/zero123_utils.py
+++ guidance/zero123_utils.py
@@ -168,7 +168,7 @@ class Zero123(nn.Module):
         target = (latents - grad).detach()
         if self.step % 10 == 0:
             save_image(self.decode_latents(target.to(torch.float16)), f"data/zero_render_{self.step}.png")
-            print(f"shape: {target.to(torch.float16))}")
+            print(f"shape: {target.to(torch.float16)}")
         
         loss = 0.5 * F.mse_loss(latents.float(), target, reduction='sum')
         
