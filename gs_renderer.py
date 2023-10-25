d8c7a0ca0d201f2c8bdefa1859904c248e991a57
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-10-25 16:48:56 +0900

    Update gs_renderer.py

--- gs_renderer.py
+++ gs_renderer.py
@@ -807,6 +807,9 @@ class Renderer:
         
         if iter > -1:
             mean_recenter = means3D - means3D.max()
+            shs = shs + mean_recenter.view(-1, 1, 3) * (500 - iter) / 500
+        else:
+            mean_recenter = means3D - means3D.max()
             shs = shs + mean_recenter.view(-1, 1, 3)
         
         # Rasterize visible Gaussians to image, obtain their radii (on screen).
